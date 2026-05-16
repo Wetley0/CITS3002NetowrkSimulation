77ab0269f9e64d3cf60d2e7fed2f9945beaeeebe
Wesley Conti <wesleyconti@Wesleys-MacBook-Pro.local>
2026-05-16 14:13:50 +0800

    Fix protocol classes

--- protocol.py
+++ protocol.py
@@ -1,25 +1,24 @@
 class Frame:
     def __init__ (self, src_mac, dest_mac, type, payload_packet):
-        src_mac = self.src_mac
-        dest_mac = self.dest_mac
-        type = self.type
-        payload_packet = self.payload_packet
+        self.src_mac = src_mac
+        self.dest_mac = dest_mac
+        self.type = type
+        self.payload_packet = payload_packet
 
 class Packet:
     def __init__ (self, src_ip, dest_ip, ttl, protocol, tot_len, payload_segment):
-        src_ip = self.src_mac
-        dest_ip = self.dest_mac
-        ttl = self.type
-        protocol = self.protocol
-        tot_len = self.tot_len
-        payload_segment = self.payload_segment
+        self.src_ip = src_ip
+        self.dest_ip = dest_ip
+        self.protocol = protocol
+        self.tot_len = tot_len
+        self.payload_segment = payload_segment
 
 class Segment:
     def __init__ (self, src_port, dest_port, length, checksum, type, seq_num, data):
-        src_port = self.src_port
-        dest_port = self.dest_port
-        length = self.length
-        checksum = self.checksum
-        type = self.type
-        seq_num = self.seq_num
-        data = self.data
+        self.src_port = src_port
+        self.dest_port = dest_port
+        self.length = length
+        self.checksum = checksum
+        self.type = type
+        self.seq_num = seq_num
+        self.data = data
